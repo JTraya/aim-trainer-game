3b272e2b9438871acbed20431f168de36e8f05d1
jtraya <jtraya7@yahoo.com>
2024-05-23 12:48:30 -0500

    added in hits and accuracy elements, updated buttons and game states, fixed known bugs

--- js/app.js
+++ js/app.js
@@ -4,14 +4,21 @@ const timeEls = document.querySelector('#time')
 const timeTextEl = document.querySelector('.timeText') 
 const targetsEls = document.querySelector('#targets')
 const game = document.querySelector('#game')
+const hitsEl = document.querySelector('#hits')
+const accuracyEl = document.querySelector('#accuracy')
 
-let time = 30
+let time = 20
 let targets = 30 
 let countdownTimer;
-
+let hits = 0
+let misses = 0
+let accuracy = '0%'
+let playing = false
 
 startBtn.addEventListener('click', () => {
     // Start the game
+    playing = true
+
     // Hide startbtn
     // document.getElementById('start-btn').style.display = 'none'
     // startBtn.style.display = 'none'
@@ -23,27 +30,35 @@ startBtn.addEventListener('click', () => {
     circle.id = 'circle'
     document.getElementById('game').appendChild(circle)
     positionCircleRandomly(circle)
+    targetsEls.innerHTML = targets - 1
+    targets = targets - 1
 
+    // console.dir(circle)
+
+    // tick down targets and add win condition
     circle.addEventListener('click', () => {
         if (targets === 0) {
+            playing = false
             clearInterval(countdownTimer);
             targetsEls.innerHTML = targets
             circle.remove();
-            // You Win!
+            // You Win! 
             const win = document.createElement('h1')
             win.id = 'win'
             win.innerText = 'You Win!'
             document.getElementById('game').appendChild(win)
         } else {
         positionCircleRandomly(circle);
+        --targets
         targetsEls.innerHTML = targets
-        --targets;
+        // --targets;
         }
 })
 
-    // Begin the countdown timer
+    // Begin the countdown timer and add lose condition
      countdownTimer = setInterval(function(){
         if (time === 0) {
+            playing = false
             circle.remove();
             clearInterval(countdownTimer);
             timeEls.innerHTML = '00:00';  
@@ -62,8 +77,11 @@ startBtn.addEventListener('click', () => {
 })
 
 resetBtn.addEventListener('click', () => {
+    // return start button, stop game, and stop timer
     startBtn.style.visibility = 'visible'
     clearInterval(countdownTimer)
+    playing = false
+    // based on condition of game clear added elements
     if (targets === 0) {
         win.remove()
     } else if (timeEls.innerHTML === '00:00') {
@@ -71,10 +89,16 @@ resetBtn.addEventListener('click', () => {
     } else {
         circle.remove()
     }
+    // reset values to normal
     targets = 30
-    time = 30
-    timeEls.innerHTML = '00:30';  
-    targetsEls.innerHTML = '30'
+    time = 20
+    hits = 0
+    misses = 0
+    accuracy = '0%'
+    accuracyEl.innerText = accuracy
+    hitsEl.innerText = hits
+    timeEls.innerHTML = '00:20';  
+    targetsEls.innerHTML = targets
 
     // if (targets !== 0 ) {
     //     circle.remove()
@@ -83,6 +107,25 @@ resetBtn.addEventListener('click', () => {
     //     targetsEls.innerHTML = '30'
     // } 
 })
+
+game.addEventListener('click', (e) => {
+    //records hits and misses
+    if (e.target.id === 'circle') {
+        hits++;
+    } else if (playing === true && e.target.id !== 'start-btn' ) {
+        misses++;
+        console.log(misses)
+    }
+    // displays hits and accuracy
+    hitsEl.innerText = hits
+    calculateAccuracy()
+})
+
+function calculateAccuracy() {
+    accuracy = (hits / (hits + misses)) * 100
+    accuracy = accuracy.toFixed(2)
+    accuracyEl.innerText = `${accuracy}%`
+}
 
 function getRandomPosition(){
     const x = Math.floor(Math.random() * (game.getBoundingClientRect().width - 100));
@@ -98,9 +141,8 @@ function positionCircleRandomly(circle) {
 
 }
 
-// console.dir(win)
-// Note for tmrw:
-// Fix bugs (of course) target window**
-// Setup Reset button
+//// Note for tmrw:
+//// Fix bugs (of course) target window**
+////Setup Reset button
 //// Add in Win/Game Over text those fulfilling conditions
-// Research and setup hits and accuracy
+//// Research and setup hits and accuracy
